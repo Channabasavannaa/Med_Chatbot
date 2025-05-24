f3e7b4c3c378a089d6d7bbb148acff149b6f10f0
chann <channabasavannamb@gmail.com>
2025-05-24 19:16:16 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical-Chatbot",
+    version = "0.0.0",
+    author = "Channa",
+    author_email="channabasavannamb@gmail.com",
+    packages = find_packages(),
+    install_requires =[]
+)
