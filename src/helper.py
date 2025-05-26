aeec932aa108e644414f85b6e1717262c843bbd0
chann <channabasavannamb@gmail.com>
2025-05-26 22:03:42 +0530

    end point

---
+++ src/helper.py
@@ -0,0 +1,30 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+
+
+
+def LoadPDF(Data):
+    loader = DirectoryLoader(Data, glob="*.pdf", loader_cls=PyPDFLoader)
+    docs = loader.load()
+    return docs
+
+
+
+
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+    chunks = text_splitter.split_documents(extracted_data)
+    return chunks
+
+
+
+
+def embedding_model():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
